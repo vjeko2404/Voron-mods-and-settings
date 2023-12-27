47a7cdc44dcfd27612af5610c1620bac70615339
vjeko2404 <96867863+vjeko2404@users.noreply.github.com>
2023-12-27 18:07:37 +0100

    Power toggle script for RPI
    I have built in relay that controls the mains of 24V and 48V PSU's
    
    This little script changes the relay state when the button is pressed for 3 seconds. 
    It also sends its state to moonraker

---
+++ Voron V2.4/gpio_toggle.py
@@ -0,0 +1,33 @@
+import requests
+from gpiozero import Button, OutputDevice
+from signal import pause
+import logging
+
+logging.basicConfig(filename='/home/pi/gpio_toggle.log', level=logging.INFO)
+
+# Setup
+button = Button(12, hold_time=3)  # 3-second hold time
+relay = OutputDevice(16, initial_value=False)
+
+moonraker_host = "http://192.168.0.192"  # Moonraker host
+moonraker_port = "7125"  # Moonraker port
+power_device_name = "Printer 24V"  # Power device name in Moonraker
+
+def update_moonraker_power_state(state):
+    url = f"http://192.168.0.192:7125/machine/device_power/device?device=Printer_24V&action={'on' if state else 'off'}"
+    data = {"action": "on" if state else "off"}
+    try:
+        response = requests.post(url)
+        logging.info(f"Making request to {url} with data {data}")
+        logging.info(f"Response: {response.status_code}, {response.text}")
+    except Exception as e:
+        logging.info(f"Failed to update Moonraker power state: {e}")
+
+def toggle_relay():
+    relay.toggle()
+    update_moonraker_power_state(relay.value)
+
+# Assign the function to the button press event
+button.when_held = toggle_relay
+
+pause()  # This will keep the script running
